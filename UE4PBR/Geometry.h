b06bafc5c9b50f752aec5505b42e51130e0f7750
shiinayuika <794191047@qq.com>
2019-08-20 10:54:50 +0800

    change struct and inheritance relationship

---
+++ UE4PBR/Geometry.h
@@ -0,0 +1,21 @@
+#include"NewObject.h"
+#pragma once
+class GeometryObject :public Object
+{
+
+};
+
+class Box :public GeometryObject
+{
+
+};
+
+class Sphere :public GeometryObject
+{
+
+};
+
+class Grid :public GeometryObject
+{
+
+};
--- UE4PBR/NewObject.h
+++ UE4PBR/NewObject.h
@@ -1,24 +1,15 @@
 #include"Transform.h"
-#include"Camera.h"
 //#include"Mesh.h"
 #pragma once
-
-struct Components
-{
-	Transform* transformation;
-	Camera* camera;
-	//Mesh* mesh;
-};
 //ª˘¿‡Object
 class Object
 {
-private:
-	Components components;
+protected:
+	Transform transformation;
 public:
+
 	Object()
 	{
-		components.transformation = new Transform();
-	}
 
-	void AddComponent();
+	}
 };
